134f68d533fe76697aaa08a2c5e9c99d33896e94
Aly <aly.ba@clearstream.com>
2020-02-04 13:01:08 +0100

    Added collection page per category

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,25 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import { Route } from "react-router-dom";
 
-import SHOP_DATA from './shop.data.js';
+import {createStructuredSelector} from 'reselect';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview';
+import {selectShopCollections} from '../../redux/shop/shop.selectors';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+import CollectionOverview from '../../components/collection-overview/collection-overview.component';
 
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
+import CollectionPage from '../collection/collection.component';
 
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = ({match}) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionOverview}/>
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+  </div>
+);
 
-export default ShopPage;
+
+const mapStateToProps = createStructuredSelector ({
+  collections: selectShopCollections
+});
+
+export default connect(mapStateToProps)(ShopPage);
